d89c3cfa6d49cbc7b5b54f39df1e48fb84dd82e4
apeksha <apeksha05hegde@gmail.com>
2022-04-09 13:11:31 +0530

    2 html pages

--- Book_a_thon/Home/urls.py
+++ Book_a_thon/Home/urls.py
@@ -1,8 +1,9 @@
 from django import views
 from django.contrib import admin
 from django.urls import path, include
+from Home import views
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("/collection", views.collection, name='collection')
+    path("collection/", views.collection, name='collection')
 ]
