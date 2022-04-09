59d46efccb1cc7388126bd3f2d88bc19b38b32a3
Restyled.io <commits@restyled.io>
2022-04-09 12:47:09 +0000

    Restyled by autopep8

--- Book_a_thon/Home/views.py
+++ Book_a_thon/Home/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
+
 def index(request):
-    return render(request,'main.html')
+    return render(request, 'main.html')
+
 
 def collection(request):
-    return render(request,'collection.html')
-
-
+    return render(request, 'collection.html')
