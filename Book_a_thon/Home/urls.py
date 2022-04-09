f07f30c6a5c24f2363910c0c6f11cd67df0ea286
Restyled.io <commits@restyled.io>
2022-04-09 12:47:13 +0000

    Restyled by black

--- Book_a_thon/Home/urls.py
+++ Book_a_thon/Home/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from Home import views
 
 urlpatterns = [
-    path("", views.index, name='home'),
-    path("collection/", views.collection, name='collection'),
+    path("", views.index, name="home"),
+    path("collection/", views.collection, name="collection"),
 ]
--- Book_a_thon/Home/views.py
+++ Book_a_thon/Home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return render(request, 'main.html')
+    return render(request, "main.html")
 
 
 def collection(request):
-    return render(request, 'collection.html')
+    return render(request, "collection.html")
