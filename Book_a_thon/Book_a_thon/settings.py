198ebb1e70895eb0e599fc663e19b2e0d04f7ad8
apeksha <apeksha05hegde@gmail.com>
2022-04-10 12:09:19 +0530

    final

--- Book_a_thon/Book_a_thon/settings.py
+++ Book_a_thon/Book_a_thon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j9!xc!cnne&k34of(zfk$9%&e5xc#k)#-q)zq5$(mg8cspqie!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel-app']
 
 
 # Application definition
